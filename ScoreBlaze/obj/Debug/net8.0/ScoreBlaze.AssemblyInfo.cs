59e238ac9fe3d21263585c6fc74ae4661d7e4a6d
Zeniff <zeniffqb.dev@gmail.com>
2025-03-05 20:07:03 -0600

    Primer commit de prueba

--- ScoreBlaze/obj/Debug/net8.0/ScoreBlaze.AssemblyInfo.cs
+++ ScoreBlaze/obj/Debug/net8.0/ScoreBlaze.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoreBlaze")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9d0ed8c4cd87a25ff0ac30a4e0e3a147a8d45c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6020bf4aa61e085115cb57b96266b350e02c8c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoreBlaze")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoreBlaze")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
